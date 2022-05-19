518ce100adf5bf46faf4d281cd1ecda9d55b6d6c
Josenene8 <100051827+Josenene8@users.noreply.github.com>
2022-05-18 21:40:42 -0300

    Update StoryMenuState.hx

--- source/StoryMenuState.hx
+++ source/StoryMenuState.hx
@@ -226,7 +226,11 @@ class StoryMenuState extends MusicBeatState
 		}
 
 		trace("Line 165");
-
+		
+                #if android
+		addVirtualPad(FULL, A_B);
+		#end
+			
 		super.create();
 	}
 
@@ -285,12 +289,12 @@ class StoryMenuState extends MusicBeatState
 					}
 				}
 
-				if (FlxG.keys.justPressed.UP)
+				if (controls.UP_P)
 				{
 					changeWeek(-1);
 				}
 
-				if (FlxG.keys.justPressed.DOWN)
+				if (controls.DOWN_P)
 				{
 					changeWeek(1);
 				}
